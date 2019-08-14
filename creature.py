783d5f1e6cc7f0364ad3efd05d309de9010e8e87
Tanmay <tanmaykumarsinha18@gmail.com>
2019-08-15 05:14:59 +0530

    Simple simulations now possible

--- creature.py
+++ creature.py
@@ -1,14 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pygame import *
 
 class Creature():
     def __init__(self,starting_pos=[0,0]):
         self.starting_pos=np.array(starting_pos)
-        self.health=100
+        self.health = 500
         self.fertility=0
-        self.pos=starting_pos
+        self.pos=np.array(starting_pos)
         self.moveflag=True
         self.content=False
+        self.theta = 2 * np.pi * np.random.rand() - np.pi
+        self.speed = 50
+        self.color = (255,9,0)
+        self.size = 5
 
     def getPos(self):
         return self.pos
@@ -16,18 +21,35 @@ class Creature():
     def move(self,worldSz):
         if(self.moveflag==True):
             self.health = self.health - 1
-            self.pos[0]=np.random.randint(-1,2)+self.pos[0]
-            self.pos[1]=np.random.randint(-1,2)+self.pos[1]
+            self.theta = self.theta + 0.5*(np.random.rand()) - 0.25
+            velocity = self.speed * np.array([np.cos(self.theta),np.sin(self.theta)])
+            self.pos = self.pos + velocity * 0.1
+            
+
+            # self.pos[0]=np.random.randint(-1,2)+self.pos[0]
+            # self.pos[1]=np.random.randint(-1,2)+self.pos[1]
             if self.pos[0]<0:
                 self.pos[0]=0
+                self.theta = self.theta - np.pi/2 
+            
             if self.pos[0]>=worldSz[0]:
                 self.pos[0]=worldSz[0]-1
+                self.theta = np.pi - self.theta
+
             if self.pos[1]<0:
                 self.pos[1]=0
+                self.theta = self.theta - np.pi/2
+
+
             if self.pos[1]>=worldSz[1]:
                 self.pos[1]=worldSz[1]-1
+                self.theta = -self.theta
 
+
+            self.pos = np.array([int(self.pos[0]),int(self.pos[1])])
+    
     def eat(self):
+        # print("EATEN")
         if (self.fertility<100):
             if (self.content==False):
                  self.content=True
@@ -41,4 +63,4 @@ class Creature():
             self.content=False
         self.fertility=0
         self.moveflag=True
-        self.pos=starting_pos
+        self.pos=self.starting_pos
--- world.py
+++ world.py
@@ -5,39 +5,109 @@ class Food():
   def __init__(self,pos):
     self.pos = pos
     self.value = 100
+    self.size = 5
+    self.color = (0,255,0)
+    self.eaten = False
 
-  def getpos(self):
+  def getPos(self):
     return self.pos
 
 class World():
   def __init__(self):
-    self.x_range = 45
-    self.y_range = 45
+    self.x_range = 600
+    self.y_range = 600
     self.grid = np.array([[" " for i in range(0,self.y_range)] for j in range(0,self.x_range)])
     self.creatures = np.array([])
     self.food = np.array([])
 
-  def print_world(self):
-    for y in self.grid:
-      for i in y:
-        print(i,end="")
-      print()
-
   def initialize_creatures(self, number):
     for i in range(0, number):
       # self.creatures.append(Creature([0,np.random.randint(0,self.y_range)]))
-      self.creatures=np.hstack((self.creatures,Creature([0,np.random.randint(0,self.y_range)])))
+      # self.creatures=np.hstack((self.creatures,Creature([0,np.random.randint(0,self.y_range)])))
+      self.creatures=np.hstack((self.creatures,Creature([300,300])))
+
+  def clear_food(self):
+    self.food = np.array([])
 
   def generate_food(self, number):
     for i in range(0,number):
       self.food=np.hstack((self.food,Food(np.array([np.random.randint(0,self.x_range),np.random.randint(0,self.y_range)]))))
       # self.food.append(Food(np.array[np.random.randint(0,self.x_range),np.random.randint(0,self.y_range)]))
+      self.food = sorted(self.food,key = lambda x:x.pos[0])
 
   def move_creatures(self):
     for creature in self.creatures:
       if creature.moveflag:
-        pos = creature.getPos()
-        self.grid[pos[0]][pos[1]] = " "
         creature.move((self.x_range,self.y_range))
-        pos = creature.getPos()
-        self.grid[pos[0]][pos[1]] = "C"
+
+
+  def print_food(self,gameDisplay):
+    for food in self.food:
+      if not food.eaten:
+        pos = food.getPos()
+        draw.rect(gameDisplay,food.color,Rect(pos[0],pos[1],food.size,food.size))
+
+
+  def print_creatures(self,gameDisplay):
+    for creature in self.creatures:
+      draw.circle(gameDisplay,creature.color,creature.getPos(),creature.size)
+
+  def reset_creatures(self):
+    new_creatures = []
+    for creature in self.creatures:
+      creature.pos = creature.starting_pos
+
+      if creature.fertility > 0:
+        new_creatures.append(Creature(creature.starting_pos))
+
+      if creature.health >= 0:
+        new_creatures.append(creature)
+
+      creature.newIteration()
+
+    self.creatures = np.array(new_creatures)
+
+  def detect_eat(self):
+    def nearest(arr,x):
+      for i in range(0,len(arr)):
+        if arr[i] >= x:
+          return i
+      
+      return -1
+
+    fud=[]
+    for f in self.food:
+      pos=f.getPos()
+      fud.append(pos[0])
+    fud=np.array(fud)
+    # print(fud)
+    for creature in self.creatures:
+      
+      pos=creature.getPos()
+      index = np.searchsorted(fud,pos[0])
+      # index = nearest(fud,pos[0])
+      # print(fud)
+      upperbound=index
+      eaten_indices = []
+      
+      while upperbound<len(fud) and fud[upperbound]<pos[0]+creature.size+2:
+      
+        if(np.linalg.norm(self.food[upperbound].pos-pos)<creature.size + self.food[0].size):
+          creature.eat()
+          self.food[upperbound].eaten = True
+          eaten_indices.append(upperbound)
+      
+        upperbound=upperbound+1
+      lowerbound=index-1
+      
+      while lowerbound>=0 and fud[lowerbound]>pos[0]-creature.size:
+      
+        if(np.linalg.norm(self.food[lowerbound].pos-pos)<creature.size + self.food[0].size):
+          creature.eat()
+          self.food[lowerbound].eaten=True
+          eaten_indices.append(lowerbound)
+      
+        lowerbound=lowerbound-1
+
+      self.food = np.delete(self.food,eaten_indices,0)
+      fud = np.delete(fud,eaten_indices,0)
