f9ac512e3151c8a905698a91a5eed6db4c5e7e26
Tanmay <tanmaykumarsinha18@gmail.com>
2019-08-14 23:52:25 +0530

    Added Food class and world class

---
+++ world.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Food():
+  def __init__(self,pos):
+    self.pos = pos
+    self.value = 100
+
+  def getpos(self):
+    return self.pos
+
+class World():
+  def __init__(self):
+    self.x_range = 45
+    self.y_range = 45
+    self.grid = np.array([["a" for i in range(0,self.y_range)] for j in range(0,self.x_range)])
+
+  def print_world(self):
+    for y in self.grid:
+      for i in y:
+        print(i,end="")
+      print()
