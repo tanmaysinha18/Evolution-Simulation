979928c7053938302c076eb2c885f9c95754b66b
kharyal <chaitanyajee@gmail.com>
2020-03-27 23:53:17 +0530

    minor changes to plots

--- Zoo/neural_network_sense/analyse.py
+++ Zoo/neural_network_sense/analyse.py
@@ -15,6 +15,8 @@ def prey_sense(prey_blocks, num_prey, numBlocksx, numBlocksy):
         # origin = [0,0]
         fig3, axs = plt.subplots(2,figsize=(8, 8))
         fig3.suptitle('prey sense plots')
+        axs[0].set_title('if predator is at (1,0)')
+        axs[1].set_title('if food is at (1,0)')
         for i in range(numBlocksx):
           for j in range(numBlocksy):
             for p in prey_blocks[i][j]:
@@ -24,15 +26,15 @@ def prey_sense(prey_blocks, num_prey, numBlocksx, numBlocksy):
                 avg_direction_vector_food = avg_direction_vector_food + y
 # [1    53,204,255]
                 axs[0].arrow(0,0,x[0],x[1],color = np.array([153,204,255])/255)
-    
+
                 axs[1].arrow(0,0,y[0],y[1],color = np.array([153,204,255])/255)
-    
+
         avg_direction_vector_pred = avg_direction_vector_pred/(num_prey+0.0001)
         avg_direction_vector_food = avg_direction_vector_food/(num_prey+0.0001)
-    
+
         axs[0].arrow(0,0,avg_direction_vector_pred[0], avg_direction_vector_pred[1],color ='r')
         axs[1].arrow(0,0,avg_direction_vector_food[0], avg_direction_vector_food[1],color ='r')   
-    
+
         plt.show()
 
 def predator_sense(predator_list):
@@ -41,7 +43,7 @@ def predator_sense(predator_list):
         avg_direction_vector = np.array([0,0])
         fig2, ax = plt.subplots(figsize=(8, 8))
         fig2.suptitle('predator sense plots')
-
+        ax.set_title('if prey is at (1,0)')
         for p in predator_list:
             x = p.compute_nn_food_custom(np.array([1,0]))
             avg_direction_vector = avg_direction_vector+x
